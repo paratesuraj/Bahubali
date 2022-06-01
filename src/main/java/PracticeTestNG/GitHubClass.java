d385dd79dd91192c0d3cc6405b71346d1fbf9abe
SURAJ PARATRE <SURAJ PARATRE@DESKTOP-V0E91U8>
2022-06-01 14:55:47 +0530

    Commit the code at 2.55pm

---
+++ src/main/java/PracticeTestNG/GitHubClass.java
@@ -0,0 +1,13 @@
+package PracticeTestNG;
+
+import org.testng.annotations.Test;
+
+public class GitHubClass 
+{
+	@Test
+	public void method1()
+	{
+		System.out.println("Practice git-hub class");
+	}
+
+}
